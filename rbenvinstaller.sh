a1c3f9bdb915ff59faed3802fb22587b872313f9
723 <rb.natsumi@gmail.com>
2016-09-28 10:53:43 +0900

    coment add

--- rbenvinstaller.sh
+++ rbenvinstaller.sh
@@ -3,6 +3,7 @@
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
  
+#bash only
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 source ~/.bash_profile
